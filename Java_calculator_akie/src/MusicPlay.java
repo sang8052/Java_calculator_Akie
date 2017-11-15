8609b220a0f12fb26cd3708a0f50d65c78302760
桑泽寰 <31176926+sang8052@users.noreply.github.com>
2017-11-16 06:19:58 +0800

    Build_1.0.0.1
    初步准备好了相关素材（背景图片、背景音乐等）。界面已经实现了对图片和音乐的显示和播放。初步完成了界面建设。

---
+++ Java_calculator_akie/src/MusicPlay.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MR_SZH
+ */
+
+
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.*;
+
+//  - 音乐播放器类 - @author lt - time 2016-7-5 来自 http://blog.csdn.net/c1481118216/article/details/51835634
+
+public class MusicPlay 
+{
+    Player player;
+    File music;
+    
+    public  MusicPlay (File file)//构造方法  参数是一个.mp3音频文件
+    {
+        this.music = file;
+    }
+    //播放方法
+    public void play() throws FileNotFoundException, JavaLayerException {
+
+            BufferedInputStream buffer = new BufferedInputStream(new FileInputStream(music));
+            player = new Player(buffer);
+            player.play();
+    }
+}
